f234a63f3e591507c257d6224b0e32b0a9749ea9
ronak6666 <ronp5678@gmail.com>
2022-08-28 18:48:32 -0400

    First core assignment

---
+++ juststarted/Core_assignments/for_loop_basic1.py
@@ -0,0 +1,36 @@
+1.
+
+# for x in range(151):
+#     print(x)
+
+
+2.
+# for x in range(5,1000,5):
+#     print(x)
+
+3.
+# for x in range(1,100):
+#     if x % 5 == 0:
+#         print ("Coding")
+#     if x % 10 == 0:
+#         print ("Coding Dojo")
+#     else:
+#         print (x)
+4.
+# sum=0
+# for x in range(0,50):
+#     if (x % 2) != 0:
+#         sum = sum + x
+#         print(sum)
+
+
+
+5.
+# for x in range(2018,0,-4):
+#     print(x)
+
+6.
+# for x in range(1,500):
+#     if x % 5 == 0:
+#         print(x)
+
